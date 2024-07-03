e48d68b9953aa655ed3014a6a646e6cd8aa0f3a4
MarcinMG-web <gierada.marcin@gmail.com>
2024-07-03 08:28:20 +0200

    feat: added neon

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -19,7 +19,29 @@ export default function Header(): JSX.Element {
     >
       {loading ? <Skeleton variant='rectangular' width={100} height={40} /> : <ColorSchemeToggle />}
 
-      <Typography level='h1' sx={{ gap: 2, display: 'flex', alignItems: 'center' }}>
+      <Typography
+        level='h1'
+        sx={{
+          gap: 2,
+          display: 'flex',
+          alignItems: 'center',
+          '@keyframes neon': {
+            '0%': {
+              textShadow:
+                '0 0 5px #ff8c00, 0 0 10px #ff8c00, 0 0 20px #ff8c00, 0 0 30px #ff8c00, 0 0 40px #ff8c00, 0 0 50px #ff8c00, 0 0 60px #ff8c00',
+            },
+            '50%': {
+              textShadow:
+                '0 0 10px #ff8c00, 0 0 20px #ff8c00, 0 0 30px #ff8c00, 0 0 40px #ff8c00, 0 0 50px #ff8c00, 0 0 60px #ff8c00, 0 0 70px #ff8c00',
+            },
+            '100%': {
+              textShadow:
+                '0 0 5px #ff8c00, 0 0 10px #ff8c00, 0 0 20px #ff8c00, 0 0 30px #ff8c00, 0 0 40px #ff8c00, 0 0 50px #ff8c00, 0 0 60px #ff8c00',
+            },
+          },
+          animation: 'neon 1.5s infinite alternate',
+        }}
+      >
         {loading ? <Skeleton variant='text' width={200} /> : 'Blog Post App'}
       </Typography>
       {loading ? <Skeleton variant='rectangular' width={150} height={40} /> : <AuthorFilter />}
