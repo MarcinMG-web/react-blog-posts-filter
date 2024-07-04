20106cfe67c39cb09771cb737e802c27f0aed020
MarcinMG-web <gierada.marcin@gmail.com>
2024-07-04 09:42:35 +0200

    style: add miss comment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // depending on your application, base can also be "/"
   base: '',
   plugins: [react()],
   server: {
